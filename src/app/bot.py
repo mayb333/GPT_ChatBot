0a2b9a4107609ea9a21a3c7a608f0916009adfb0
Sokolov Fedor <sokol_f4@mail.ru>
2023-10-24 00:22:57 +0300

    Added insertion new users to users table

--- src/app/bot.py
+++ src/app/bot.py
@@ -1,8 +1,8 @@
 import os
-from aiogram import Bot, Dispatcher
-from aiogram.utils import executor
+from aiogram import Bot, Dispatcher, executor, types
 from dotenv import load_dotenv
 from loguru import logger
+from src.database import DataBase
 
 
 # Load environment variables
@@ -13,6 +13,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+db = DataBase()
 
 
 class GptBotLaunching:
@@ -27,3 +28,18 @@ class GptBotLaunching:
 
     async def _on_shutdown(self, dp):
         logger.info("Bot stopped.")
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Hello! Ask me something")
+    
+    logger.info(f"User with id={message.from_id} started the bot!")
+
+    user_info = message.from_user
+    user_id, username, first_name = user_info.id, user_info.username, user_info.first_name
+
+    # Add new user to users table
+    if db.user_not_in_users(user_id=user_id):
+        db.add_user(user_id=user_id, username=username, first_name=first_name)
+    
--- src/database/database.py
+++ src/database/database.py
@@ -1,5 +1,6 @@
 import os
 import psycopg2
+from datetime import datetime
 from loguru import logger
 from dotenv import load_dotenv
 
@@ -15,9 +16,50 @@ class DataBase:
     def __init__(self, database_url: str = DATABASE_URL):
         self.database_url = database_url
 
+        logger.info("Initialized Database")
+
     def connect_to_db(self):
         connection = psycopg2.connect(self.database_url)
 
         logger.info("Connected to Database")
 
         return connection 
+    
+    def add_user(self, user_id, username, first_name, timestamp=datetime.now().strftime("%d/%m/%Y %H:%M:%S")):
+        try:
+            connection = self.connect_to_db()
+
+            with connection.cursor() as cursor:
+                query = f"""INSERT INTO users (user_id, username, first_name, first_message) 
+                            VALUES('{user_id}', '{username}', '{first_name}', '{timestamp}')"""
+
+                cursor.execute(query)
+                connection.commit()
+                connection.close()
+
+                logger.info(f"User with id={user_id} has been added to users table")
+                logger.info("Closed connection to Database")
+        except Exception as exp:
+            logger.info(f"Couldn't execute the function \"add_user\" \n {exp}")
+
+    def user_not_in_users(self, user_id):
+        try:
+            connection = self.connect_to_db()
+            flag = False
+
+            with connection.cursor() as cursor:
+                query = f"""SELECT user_id
+                            FROM users
+                            WHERE user_id = '{user_id}';"""
+                cursor.execute(query)
+                result = cursor.fetchone()
+
+                connection.close()
+                logger.info("Closed connection to Database")
+
+                if result is None:
+                    flag = True
+                return flag
+
+        except Exception as exp:
+            logger.info(f"Couldn't execute the function \"user_not_in_users\" \n {exp}")

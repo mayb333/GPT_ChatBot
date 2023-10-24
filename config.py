cc2833e94b913e06ce9fc3bd3821c085ab19bb76
Sokolov Fedor <sokol_f4@mail.ru>
2023-10-25 00:49:04 +0300

    Edited config.py

--- config.py
+++ config.py
@@ -8,3 +8,5 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 CONTACT_ACCOUNT = os.getenv("CONTACT_ACCOUNT")
+ADMIN_IDS = [int(item.strip()) for item in os.getenv("ADMIN_IDS_LIST").split(',')]
+ALLOWED_USERS = [int(item.strip()) for item in os.getenv("ALLOWED_USERS_LIST").split(',')]

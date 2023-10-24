6eb75ae97cfac3e8c6f5a3cc27fb723755c53ddb
Sokolov Fedor <sokol_f4@mail.ru>
2023-10-24 14:20:06 +0300

    Separated handlers from main bot file

--- src/app/bot.py
+++ src/app/bot.py
@@ -2,18 +2,8 @@ import os
 from aiogram import Bot, Dispatcher, executor, types
 from dotenv import load_dotenv
 from loguru import logger
-from src.database import DataBase
-
-
-# Load environment variables
-load_dotenv()
-
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
-db = DataBase()
+from src.app.loader import bot, dp, db
+from src.handlers.start import process_start_command
 
 
 class GptBotLaunching:
@@ -31,15 +21,5 @@ class GptBotLaunching:
 
 
 @dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-    await message.reply("Hello! Ask me something")
-    
-    logger.info(f"User with id={message.from_id} started the bot!")
-
-    user_info = message.from_user
-    user_id, username, first_name = user_info.id, user_info.username, user_info.first_name
-
-    # Add new user to users table
-    if db.user_not_in_users(user_id=user_id):
-        db.add_user(user_id=user_id, username=username, first_name=first_name)
-    
+async def on_start(message: types.Message):
+    await process_start_command(message)
---
+++ src/handlers/start.py
@@ -0,0 +1,17 @@
+from aiogram import types
+from loguru import logger
+from src.app.loader import dp, db
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Hello! Ask me something")
+    
+    logger.info(f"User with id={message.from_id} started the bot!")
+
+    user_info = message.from_user
+    user_id, username, first_name = user_info.id, user_info.username, user_info.first_name
+
+    # Add new user to users table
+    if db.user_not_in_users(user_id=user_id):
+        db.add_user(user_id=user_id, username=username, first_name=first_name)
